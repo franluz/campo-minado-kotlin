59c45e0fd372c409a0d6af775a1058db40e29f09
Francielle Luz <francielle.luz@gft.com>
2019-07-25 14:59:40 -0300

    inclusao de tabuleiro

---
+++ src/modelo/Tabuleiro.kt
@@ -0,0 +1,37 @@
+package modelo
+
+import kotlin.reflect.jvm.internal.impl.types.checker.TypeCheckingProcedureCallbacks
+
+enum class TabuleiroEvento(VITORIA,DERROTA)
+class Tabuleiro(val qtdLinhas:Int, val qntColunas:Int, private val qtdMinas:Int){
+    private val campos = ArrayList<ArrayList<Campo>>()
+    private val callbacks = ArrayList<(TabuleiroEvento)->Unit>()
+    init {
+        gerarCampos()
+        associarVizinhos()
+        sortearMinas()
+    }
+    private fun gerarCampos(){
+        for(linha in 0 util qtdLinhas){
+            campos.add(ArrayList())
+            for(coluna in 0 until qntColunas){
+                val campoNovo = Campo(linha,coluna)
+                campos[linha].add(campoNovo)
+            }
+        }
+    }
+    private fun associarVizinhos(){
+        forEachCampos { associarVizinhos(it) }
+    }
+    private fun associarVizinhos(campo: Campo){
+        var (linha,coluna) = campo
+        val linhas = arrayListOf(linha-1,linha,linha+1)
+        val colunas = arrayListOf(coluna-1,coluna, coluna+1)
+    }
+    private fun sortearMinas(){
+
+    }
+    fun forEachCampos(callback: (Campo)->Unit){
+        campos.forEach{linha -> linha.forEach(callback)}
+    }
+}
